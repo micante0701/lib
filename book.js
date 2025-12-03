cffec7a405112a967ec7eb0dcebfa132a22f1eaf
micante0701 <fortry456@yahoo.com.tw>
2025-12-03 08:34:48 +0800

    Add files via upload

--- book.js
+++ book.js
@@ -1,29 +1,3 @@
-
-
-// ↓↓↓ 自定義背景色(程式碼Start) ↓↓↓
-// // 背景顏色設定
-// const colorInput = document.getElementById("colorPicker");
-// // 取得 body 的目前背景顏色
-// let currentColor = window.getComputedStyle(document.body).backgroundColor;
-// // 把 rgb(...) 轉成 hex (#RRGGBB)
-// function rgbToHex(rgb) {
-//     const result = rgb.match(/\d+/g).map(Number); // 取出數字
-//     return (
-//         "#" +
-//         result
-//             .slice(0, 3) // 只取 R、G、B
-//             .map(x => x.toString(16).padStart(2, "0"))
-//             .join("")
-//     );
-// }
-// // 設定 color input 的初始值
-// colorInput.value = rgbToHex(currentColor);
-// // 綁定事件：選擇顏色時更新背景
-// colorInput.addEventListener("input", function () {
-//     document.body.style.backgroundColor = this.value;
-// });
-// ↑↑↑ 自定義背景色(程式碼End) ↑↑↑
-
 // ↓↓↓ 護眼模式切換(程式碼Start) ↓↓↓
 
 // 載入時偵測 localStorage
@@ -59,20 +33,35 @@ function applyTheme() {
         btn.textContent = "護眼模式";
     }
 }
+
 // ↑↑↑ 護眼模式切換(程式碼End) ↑↑↑
 
-/* // 打開彈出視窗
-function openPopup() {
-    document.getElementById("overlay").style.display = "flex";
-}
+// ↓↓↓ 自定義背景色(程式碼Start) ↓↓↓
+// // 背景顏色設定
+// const colorInput = document.getElementById("colorPicker");
+// // 取得 body 的目前背景顏色
+// let currentColor = window.getComputedStyle(document.body).backgroundColor;
+// // 把 rgb(...) 轉成 hex (#RRGGBB)
+// function rgbToHex(rgb) {
+//     const result = rgb.match(/\d+/g).map(Number); // 取出數字
+//     return (
+//         "#" +
+//         result
+//             .slice(0, 3) // 只取 R、G、B
+//             .map(x => x.toString(16).padStart(2, "0"))
+//             .join("")
+//     );
+// }
+// // 設定 color input 的初始值
+// colorInput.value = rgbToHex(currentColor);
+// // 綁定事件：選擇顏色時更新背景
+// colorInput.addEventListener("input", function () {
+//     document.body.style.backgroundColor = this.value;
+// });
 
-// 關閉彈出視窗
-function closePopup(event) {
-    // 檢查點擊是否在 overlay 上
-    if (event.target.id === "overlay") {
-        document.getElementById("overlay").style.display = "none";
-    }
-} */
+// ↑↑↑ 自定義背景色(程式碼End) ↑↑↑
+
+// ↓↓↓ Popup(程式碼Start) ↓↓↓
 
 // 切換彈出視窗 (按鈕呼叫)
 function togglePopup() {
@@ -124,27 +113,6 @@ function changeFontSize(delta) {
         p.style.fontSize = newSize + "px";
     });
 }
-
-/* 產生文字目錄 */
-/* HTML搭配<nav id="toc"></nav> */
-/* const toc = document.getElementById("toc");
-document.querySelectorAll("h5").forEach((h, i) => {
-    // 建立唯一 id
-    const id = "section-" + i;
-    h.id = id;
-
-    // 直接取整個 h5 的文字
-    const text = h.textContent;
-
-    // 建立目錄連結
-    const link = document.createElement("a");
-    link.href = "#" + id;
-    link.textContent = text;
-
-    // 加到目錄
-    toc.appendChild(link);
-    toc.appendChild(document.createElement("br")); // 換行
-}); */
 
 /* 針對<h5>產生目錄標題 */
 const toc = document.getElementById("toc");
