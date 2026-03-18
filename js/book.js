ba299514b3b84b417902dab885f9e19ac61454ab
micante0701 <fortry456@yahoo.com.tw>
2026-03-18 10:06:19 +0800

    Add files via upload

--- js/book.js
+++ js/book.js
@@ -1,7 +1,7 @@
 // Service Worker
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register("service.js")
+        navigator.serviceWorker.register("service.js", { scope: "./" })
             .then(reg => console.log("Service Worker Registered:", reg))
             .catch(err => console.error("Service Worker Registration Failed:", err));
     });
