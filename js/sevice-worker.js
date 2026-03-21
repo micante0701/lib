2ab2df967f7571e36a030707a9e3e040b3d4530d
micante0701 <fortry456@yahoo.com.tw>
2026-03-22 05:39:17 +0800

    Delete js/sevice-worker.js

--- js/sevice-worker.js
+++
@@ -1,8 +0,0 @@
-// Service Worker
-// if ("serviceWorker" in navigator) {
-//     window.addEventListener("load", () => {
-//         navigator.serviceWorker.register("service.js")
-//             .then(reg => console.log("Service Worker Registered:", reg))
-//             .catch(err => console.error("Service Worker Registration Failed:", err));
-//     });
-// }
